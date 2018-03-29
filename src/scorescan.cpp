1b9f611fb7e51131266f146051ae3a75187c8034
Arlo Siemsen <arlo@arloworks.com>
2018-03-28 20:55:47 -0700

    WIP

--- src/scorescan.cpp
+++ src/scorescan.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 #include <zbar.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <iostream>
-#include <string>
+
+#include "nanosvg.h"
+//#include "json.hpp"
 
 using namespace std;
 using namespace cv;
 using namespace zbar;
-
-RNG rng(12345);
+//using namespace nlohmann;
 
 void sortPointsCW(vector<Point>& points)
 {
@@ -30,7 +33,10 @@ vector<Point2f> rectCorners(Rect2f rect)
 	};
 }
 
-
+Rect2f rectFromCorners(NSVGshape* shape)
+{
+	return {shape->bounds[0], shape->bounds[1], (shape->bounds[2] - shape->bounds[0]), (shape->bounds[3] - shape->bounds[1])};
+}
 
 bool tryFindPage(
 		Mat inPage,
@@ -39,7 +45,6 @@ bool tryFindPage(
 		Size pageSize,
 		Rect2f qrBox)
 {
-
 	// Zoom out to ensure we include the full box
 	double scaleFactor = 0.7;
 
@@ -61,13 +66,19 @@ bool tryFindPage(
 		cv::warpPerspective(inPage, warped, scalePerspective, pageSize);
 	}
 
+	//imshow( "Display window", warped ); waitKey(0);
+
 	// Blur the inPage.
 	Mat blurred;
-	cv::GaussianBlur(warped, blurred, Size {3,3}, 0);
+	cv::GaussianBlur(warped, blurred, {}, 1, 1);
+
+	//imshow( "Display window", blurred ); waitKey(0);
 
 	// Find edges
 	Mat edged;
 	cv::Canny(blurred, edged, 75, 200);
+
+	//imshow( "Display window", edged ); waitKey(0);
 
 	// Find contours.
 	vector<vector<Point>> contours{};
@@ -99,7 +110,6 @@ bool tryFindPage(
 
 			auto finalPerspective = cv::getPerspectiveTransform(srcCorners, pageCorners);
 
-
 			cv::warpPerspective(inPage, outPage, finalPerspective * scalePerspective, pageSize);
 			return true;
 		}
@@ -110,14 +120,65 @@ bool tryFindPage(
 
 int main (int argc, char **argv)
 {
+	const char* windowName = "PineScan";
+	namedWindow(windowName, WINDOW_NORMAL);
+
+	vector<vector<Point>> shapes;
+	vector<string> ids;
+	Size pageSize;
+	Rect2f qrBox;
+
+	{
+		NSVGimage* image = NULL;
+		int w, h;
+		const char* filename = "/home/arlo/git/scorescan-c/scorescan/src/sheet.svg";
+
+		printf("parsing %s\n", filename);
+		image = nsvgParseFromFile(filename, "px", 150.0f);
+		if (image == NULL) {
+			printf("Could not open SVG image.\n");
+		}
+
+		for (auto shape = image->shapes; shape != NULL; shape = shape->next) {
+			string id(shape->id);
+
+			if (id == "page")
+			{
+				pageSize = {static_cast<int>(shape->bounds[2]), static_cast<int>(shape->bounds[3])};
+				cout << "Page size detected " << pageSize << endl;
+			}
+			else if (id == "qr")
+			{
+				qrBox = rectFromCorners(shape);
+				cout << "QR detected " << shape << endl;
+			}
+
+			//cout << "Shape #" << shape->id << ": box: " << shape->bounds[0] << ", " << shape->bounds[1] << ", "<< shape->bounds[2] << ", "<< shape->bounds[3] << endl;
+			vector<Point> object{};
+
+			for (auto path = shape->paths; path != NULL; path = path->next) {
+				for (auto i = 0; i < path->npts; i++) {
+					object.push_back(Point2f(path->pts[i*2], path->pts[i*2 + 1]));
+				}
+			}
+			shapes.push_back(object);
+			ids.push_back(id);
+		}
+
+		w = (int)image->width;
+		h = (int)image->height;
+		cout << "width" << w << "height" << h << endl;
+
+		nsvgDelete(image);
+	}
+
     string imageName("/home/arlo/git/scorescan-c/scorescan/src/scan.jpg"); // by default
-    if( argc > 1)
-    {
-        imageName = argv[1];
-    }
 
     Mat rawImage;
     rawImage = imread(imageName.c_str(), IMREAD_GRAYSCALE); // Read the file
+
+    Mat threshold;
+    cv::threshold(rawImage, threshold, 128, 255, ThresholdTypes::THRESH_BINARY);
 
     if(rawImage.empty())
     {
@@ -133,10 +194,13 @@ int main (int argc, char **argv)
     scanner.set_config(ZBAR_QRCODE, ZBAR_CFG_POSITION, 1);
 
     // wrap image data
-    Image zimage(rawImage.cols, rawImage.rows, "Y800", rawImage.data, rawImage.rows * rawImage.cols);
+    Image zimage(threshold.cols, threshold.rows, "Y800", threshold.data, threshold.rows * threshold.cols);
 
     // scan the image for barcodes
     scanner.scan(zimage);
+
+
+    int qrCount = 0;
     for(Image::SymbolIterator symbol = zimage.symbol_begin(); symbol != zimage.symbol_end(); ++symbol)
     {
         if (symbol->get_type() == ZBAR_QRCODE)
@@ -149,20 +213,50 @@ int main (int argc, char **argv)
 				{ static_cast<float>(symbol->get_location_x(1)), static_cast<float>(symbol->get_location_y(1)) }
         	};
 
-        	string data = symbol->get_data();
+        	qrCount++;
 
-        	Size pageSize {359, 480};
-        	Point2f qrLocation {5, 5};
-        	Rect2f qrBox {5, 5, 75, 75};
+        	string data = symbol->get_data();
 
         	Mat warped;
         	if (tryFindPage(rawImage, warped, qrCorners, pageSize, qrBox))
         	{
-        		imshow( "Display window", warped );
-        		waitKey(0);
+        		Mat thresholded;
+        		cv::threshold(warped, thresholded, 0, 255, ThresholdTypes::THRESH_BINARY_INV | ThresholdTypes::THRESH_OTSU);
+
+        		//imshow( "Display window", thresholded ); waitKey(0);
+
+        		assert(shapes.size() == ids.size());
+        		for (size_t i = 0; i < shapes.size(); i++)
+        		{
+        			Mat mask = Mat::zeros(thresholded.rows, thresholded.cols, thresholded.type());
+        			cv::drawContours(mask, shapes, i, 255, -1);
+
+        			const int expand_px = 5;
+        			auto kernel = Mat::ones({expand_px, expand_px}, CV_8U);
+
+        			Mat eroded;
+        			cv::dilate(mask, eroded, kernel);
+
+        			double filled = cv::mean(thresholded, mask).val[0] / 255.0;
+
+        			//float filled = static_cast<float>(cv::countNonZero(masked)) / static_cast<float>(cv::countNonZero(mask));
+
+        			if (filled > 0.3)
+        			{
+        				cv::drawContours(warped, shapes, i, 0, 4);
+        			}
+
+        			cout << ids[i] << ": " << filled << endl;
+
+        		}
+
+        		//imshow(windowName, warped ); waitKey(0);
+        		//break;
         	}
         }
     }
+
+    cout << "Found " << qrCount << " QR codes" << endl;
 
     // clean up
     zimage.set_data(NULL, 0);
